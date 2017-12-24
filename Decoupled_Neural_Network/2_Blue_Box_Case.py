cba775fa7b14c14bd6f1b6d6c4e69fe71ed0e8d0
Jae Duk Seo <JaeDukSeo@users.noreply.github.com>
2017-12-24 10:45:07 -0500

    Update 2_Blue_Box_Case.py

--- Decoupled_Neural_Network/2_Blue_Box_Case.py
+++ Decoupled_Neural_Network/2_Blue_Box_Case.py
@@ -1,8 +1,10 @@
 
-import numpy as np
-import sys
 
-import multiprocessing
+import numpy as np
+import sys,time,multiprocessing
+
+
+
 
 def generate_dataset(output_dim = 8,num_examples=1000):
     def int2vec(x,dim=output_dim):
@@ -48,7 +50,7 @@ iterations = 2000
 x,y = generate_dataset(num_examples=num_examples, output_dim = output_dim)
 
 batch_size = 1000
-alpha = 0.0003
+alpha = 0.03
 
 
 class DNI(object):
@@ -77,11 +79,22 @@ class DNI(object):
     def update_synthetic_weights(self,true_gradient):
         self.synthetic_gradient_delta = self.synthetic_gradient - true_gradient
         self.weights_synthetic_grads += self.output.T.dot(self.synthetic_gradient_delta) * self.alpha
-        
+
+def layer_1_update_synthetic_weights():
+    layer_1.update_synthetic_weights(layer_1_delta)
+
+def layer_2_update_synthetic_weights():
+    layer_2.update_synthetic_weights(layer_2_delta)
+
+def layer_3_update_synthetic_weights():
+    layer_3.update_synthetic_weights(layer_3_delta)
+    
+
 if __name__ == '__main__':
     multiprocessing.freeze_support()
 
     #  input = 24, output = 12 , layer_1_dim = 128, layer_2_dim = 64
+    start = time.time()
     input_dim = len(x[0])
     layer_1_dim = 128
     layer_2_dim = 64
@@ -90,14 +103,6 @@ if __name__ == '__main__':
     layer_2 = DNI(layer_1_dim,layer_2_dim,sigmoid,sigmoid_out2deriv,alpha)
     layer_3 = DNI(layer_2_dim, output_dim,sigmoid, sigmoid_out2deriv,alpha)
 
-    def layer_1_update_synthetic_weights():
-        layer_1.update_synthetic_weights(layer_1_delta)
-
-    def layer_2_update_synthetic_weights():
-        layer_2.update_synthetic_weights(layer_2_delta)
-
-    def layer_3_update_synthetic_weights():
-        layer_3.update_synthetic_weights(layer_3_delta)
 
     for iter in range(iterations):
         error = 0
@@ -122,7 +127,7 @@ if __name__ == '__main__':
         
         error += (np.sum(np.abs(layer_3_delta * layer_3_out * (1 - layer_3_out))))
 
-        if(error < 0.000001):
+        if(error < 0.1):
             sys.stdout.write("\rIter:" + str(iter) + " Loss:" + str(error))
             break       
             
@@ -130,7 +135,8 @@ if __name__ == '__main__':
         if(iter % 100 == 0):
             print("")
 
-    print("\nFinal Prediction : ")
+    end = time.time()
+    
 
     _, layer_1_out = layer_1.forward_and_synthetic_update(x)
     layer_1_delta, layer_2_out = layer_2.forward_and_synthetic_update(layer_1_out)
@@ -143,6 +149,6 @@ if __name__ == '__main__':
         print(layer_3_out[iter].dot(2**np.arange(x[iter][:12].size)[::-1]))
 
         truteh = x[iter][:12] + x[iter][12:]
-        print(truteh.dot(2**np.arange(x[iter][:12].size)[::-1]),'\n')
+        print("The truth data: ",truteh.dot(2**np.arange(x[iter][:12].size)[::-1]),'\n')
         
-        
+    print("\n\n------------\nTraining Time: ",end - start )     
